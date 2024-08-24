60d59c55bdf12877e263b37374b654b68cb9a55a
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-25 02:13:16 +0530

    updated

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -3,25 +3,24 @@ import './Login.css'
 const Login = () => {
   return (
     <div className="login-container">
-    <div className="login-box">
-      <h2>Login</h2>
-      <form>
-        <div className="input-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" placeholder="Value" />
+      <div className="login-box">
+      
+        <form>
+          <div className="input-group">
+            <label>Email</label>
+            <input type="email" placeholder="Value" />
+          </div>
+          <div className="input-group">
+            <label>Password</label>
+            <input type="password" placeholder="Value" />
+          </div>
+          <button type="submit" className="login-button">Sign In</button>
+        </form>
+        <div className="login-footer">
+          
         </div>
-        <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="Value" />
-        </div>
-        <button type="submit" className="login-button">Sign In</button>
-      </form>
-      <div className="login-footer">
-        <a href="#">Forgot password?</a>
-        <a href="#">Sign Up</a>
       </div>
     </div>
-  </div>
   )
 }
 
