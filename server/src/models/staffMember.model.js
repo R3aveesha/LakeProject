e9f0610fd870702033d8c9b1dddcbffb333277e1
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-08-25 15:51:18 +0530

    Staff member controller cmp

--- server/src/models/staffMember.model.js
+++ server/src/models/staffMember.model.js
@@ -1,32 +1,37 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const argon2 = require('argon2');
+const argon2 = require("argon2");
 
-const StaffMemberSchema = new Schema({
+const StaffMemberSchema = new Schema(
+  {
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     role: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
     salary: { type: Number, required: true },
-    leaves: [{
+    leaves: [
+      {
         startDate: { type: Date, required: true },
         endDate: { type: Date, required: true },
-        reason: { type: String, required: true }
-    }]
-},{timestamps: true});
+        reason: { type: String, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 /**
  * Hash password before saving the StaffMember
  * @param {Object} next - The next function in the middleware chain
  */
-StaffMemberSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) return next();
-    try {
-        this.password = await argon2.hash(this.password);
-        next();
-    } catch (err) {
-        next(err);
-    }
+StaffMemberSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await argon2.hash(this.password);
+    next();
+  } catch (err) {
+    next(err);
+  }
 });
 
 /**
@@ -34,6 +39,8 @@ StaffMemberSchema.pre('save', async function(next) {
  * @param {String} password - The password to compare with the stored hash
  * @returns {Promise<Boolean>} A promise that resolves with a boolean indicating if the password matches
  */
-StaffMemberSchema.methods.verifyPassword = async function(password) {
-    return argon2.verify(this.password, password);
-}
+StaffMemberSchema.methods.verifyPassword = async function (password) {
+  return argon2.verify(this.password, password);
+};
+
+module.exports = mongoose.model("StaffMember", StaffMemberSchema);
