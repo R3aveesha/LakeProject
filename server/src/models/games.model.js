e6dae468489555f6d8999ff5b1bab63fe223c048
MALLAWARACHCHI.U.V.N it22092274 <it22092274@my.sliit.lk>
2024-08-25 14:33:29 +0530

    backend commit one

---
+++ server/src/models/games.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const GameSchema = new Schema({
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    availableTimes: [{ type: Date }],
+    ratings: [{
+        customerId: { type: Schema.Types.ObjectId, ref: 'Customer' },
+        score: { type: Number, required: true },
+        feedback: { type: String }
+    }],
+    createdAt: { type: Date, default: Date.now },
+    images: { type: [String], default: [] }
+});
+
+module.exports = mongoose.model('Game', GameSchema);
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,15 @@
 const express = require("express");
+
+const gameRoutes = require('./games.route.js');
+const authRoutes = require('./auth.route.js');
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+router.use("/games", gameRoutes);
+router.use("/auth",authRoutes);
 
 module.exports = router;
