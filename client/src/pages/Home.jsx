b8868e855e0b5932cee54b2d21277873e3f25a90
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-26 11:27:56 +0530

    up

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import NavBar from '../components/core/NavBar';
 import Sidebar from '../components/core/Sidebar';
 import NavFunction from '../functions/navFunction';
+import StaffDashboard from './staffManagement/StaffDashboard';
 
 const Home = () => {
 
   return (
     <section>
       <NavFunction name={'home'} />
+      <StaffDashboard />
     </section>
 
   );
