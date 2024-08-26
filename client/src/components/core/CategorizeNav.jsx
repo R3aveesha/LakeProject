214a4a8106bde3ba50fb2c58d97581c261f3c789
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-08-27 00:59:40 +0530

    cmm

--- client/src/components/core/CategorizeNav.jsx
+++ client/src/components/core/CategorizeNav.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
-import { FaBars } from 'react-icons/fa'; // Using react-icons for the menu icon
+import React, { useState } from "react";
+import { FaBars } from "react-icons/fa";
 
-const CategorizeNav = () => {
+const CategorizeNav = ({
+  onCategoryClick,
+  onIndoorClick,
+  onOutdoorClick,
+  onWaterClick,
+}) => {
   const [isOpen, setIsOpen] = useState(true);
 
   const toggleSidebar = () => {
@@ -9,64 +14,74 @@ const CategorizeNav = () => {
   };
 
   const sidebarStyle = {
-    backgroundColor: '#1b1f38',
-    color: 'white',
-    padding: isOpen ? '20px' : '10px',
-    borderRadius: '10px',
-    width: isOpen ? '300px' : '60px',
-    height: 'calc(100vh - 40px)',
-    fontFamily: 'Arial, sans-serif',
-    textAlign: 'center',
-    transition: 'width 0.3s ease',
-    display: 'flex',
-    flexDirection: isOpen ? 'column' : 'column',
-    alignItems: 'center',
+    backgroundColor: "#1b1f38",
+    color: "white",
+    padding: isOpen ? "20px" : "10px",
+    borderRadius: "10px",
+    width: isOpen ? "300px" : "60px",
+    height: "calc(100vh - 40px)",
+    fontFamily: "Arial, sans-serif",
+    textAlign: "center",
+    transition: "width 0.3s ease",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   };
 
   const containerStyle = {
-    position: 'relative',
-    display: 'flex',
-    alignItems: 'center',
+    position: "relative",
+    display: "flex",
+    alignItems: "center",
   };
 
   const toggleButtonStyle = {
-    display: isOpen ? 'none' : 'block',
-    fontSize: '2rem',
-    cursor: 'pointer',
-    position: 'absolute',
-    top: '15px',
-    right: '15px',
+    display: isOpen ? "none" : "block",
+    fontSize: "2rem",
+    cursor: "pointer",
+    position: "absolute",
+    top: "15px",
+    right: "15px",
   };
 
   const headingStyle = {
-    display: isOpen ? 'block' : 'none',
+    display: isOpen ? "block" : "none",
+    cursor: "pointer",
   };
 
   const listStyle = {
-    display: isOpen ? 'block' : 'none',
-    listStyleType: 'none',
+    display: isOpen ? "block" : "none",
+    listStyleType: "none",
     padding: 0,
     margin: 0,
   };
 
   const listItemStyle = {
-    marginBottom: '15px', // Adjust this value to control the space between list items
-    fontSize: '18px',
+    marginBottom: "15px",
+    fontSize: "18px",
+    cursor: "pointer",
   };
 
   const linkStyle = {
-    color: 'white', // Set the link color to white
-    textDecoration: 'none', // Remove the underline
+    color: "white",
+    textDecoration: "none",
   };
 
   return (
     <div style={containerStyle}>
       <div style={sidebarStyle}>
-        <h2 style={headingStyle}>Categorize</h2>
+        <h2 style={headingStyle} onClick={onCategoryClick}>
+          Categorize
+        </h2>
         <ul style={listStyle}>
-          <a href='' style={linkStyle}><li style={listItemStyle}>Indoor Games</li></a>
-          <a href='' style={linkStyle}><li style={listItemStyle}>Outdoor Games</li></a>
-          <a href='' style={linkStyle}> <li style={listItemStyle}>Water Games</li></a>
+          <li style={listItemStyle} onClick={onIndoorClick}>
+            Indoor Games
+          </li>
+          <li style={listItemStyle} onClick={onOutdoorClick}>
+            Outdoor Games
+          </li>
+          <li style={listItemStyle} onClick={onWaterClick}>
+            Water Games
+          </li>
         </ul>
       </div>
       <div style={toggleButtonStyle} onClick={toggleSidebar}>
