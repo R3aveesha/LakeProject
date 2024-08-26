570f3ef4dc2a2424cd38823299ea483435f0d67c
shashitha2002 <130375872+shashitha2002@users.noreply.github.com>
2024-08-26 11:14:56 +0530

    8th commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx'
+import Home from './pages/Home.jsx';
+import FoodOrderStart from './pages/foodManagement/FoodOrderStart.jsx';
 //import AdminLogins from './components/logins/AdminLogins.jsx';
 //import IndoorGames from './pages/gamesManagement/IndoorGames.jsx';
 //import LoginPage from './components/logins/LoginPage.jsx';
@@ -15,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
 
         {/* <Route path="/games/indoor" element={<IndoorGames />} /> */}
-        
+        <Route path="/food/start" element={<FoodOrderStart />} />
       </Routes>
     </Router>
   );
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import NavBar from '../components/core/NavBar';
 import Sidebar from '../components/core/Sidebar';
 import NavFunction from '../functions/navFunction';
-
+import Footer from '../components/core/Footer';
 const Home = () => {
 
   return (
     <section>
       <NavFunction name={'home'} />
+      <Footer/>
     </section>
+    
   );
 };
 
