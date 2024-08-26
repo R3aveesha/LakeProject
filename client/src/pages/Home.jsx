8372e347ad94b14886173a6363be722c763a33a8
it22092274 <128246276+it22092274@users.noreply.github.com>
2024-08-26 11:57:52 +0530

    dahawana komittuwa
    onna dammo

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,13 +5,14 @@ import NavFunction from '../functions/navFunction';
 import StaffRegistrationForm from '../pages/staffManagement/StafftRegForm';
 
 import Footer from '../components/core/Footer';
+import Carousel from '../components/core/HomeCarousel';
 const Home = () => {
 
   
   const images = [
-    'https://via.placeholder.com/600x400?text=Slide+1',
-    'https://via.placeholder.com/600x400?text=Slide+2',
-    'https://via.placeholder.com/600x400?text=Slide+3',
+    '/image1.jpg',
+    '/image2.jpg',
+    'image3.jpg',
   ];
   
 
@@ -19,6 +20,7 @@ const Home = () => {
     <section>
       <NavFunction name={'home'} />
       <StaffRegistrationForm/>
+      <Carousel images={images} />
       <Footer/>
     </section>
     
