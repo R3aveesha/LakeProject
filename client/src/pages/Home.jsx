c00e59624ef9eaa987931538a7524770cfa38d52
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-26 11:54:10 +0530

    updated

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,22 +2,16 @@ import React from 'react';
 import NavBar from '../components/core/NavBar';
 import Sidebar from '../components/core/Sidebar';
 import NavFunction from '../functions/navFunction';
-<<<<<<< HEAD
-import StaffDashboard from './staffManagement/StaffDashboard';
+import StaffRegistrationForm from '../pages/staffManagement/StafftRegForm';
 
-=======
 import Footer from '../components/core/Footer';
->>>>>>> f80b15bce16aafa3870b0ff47a495bff4bd4e439
 const Home = () => {
 
   return (
     <section>
       <NavFunction name={'home'} />
-<<<<<<< HEAD
-      <StaffDashboard />
-=======
+      <StaffRegistrationForm/>
       <Footer/>
->>>>>>> f80b15bce16aafa3870b0ff47a495bff4bd4e439
     </section>
     
 
