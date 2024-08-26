5a4f80846b416f30ca0fe2b7017740ab33f3e49f
Praboth Sandaruwan <bleezbub12@gmail.com>
2024-08-26 05:52:41 +0530

    dyn table cmpl

--- client/src/components/reUseable/DynamicTable.jsx
+++ client/src/components/reUseable/DynamicTable.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-const DynamicTable = ({ headers, data }) => {
+const DynamicTable = ({
+  headers,
+  data,
+  headerToKeyMapping, // map object {Header:DataKey}
+  opt,
+  onDelete = () => {},
+  onUpdate = () => {},
+}) => {
   return (
     <table>
       <thead>
@@ -8,14 +15,44 @@ const DynamicTable = ({ headers, data }) => {
           {headers.map((header, index) => (
             <th key={index}>{header}</th>
           ))}
+          {opt === 0 && <th>Edit</th>}
+          {opt === 1 && <th>Delete</th>}
+          {opt === 2 && (
+            <>
+              <th>Edit</th>
+              <th>Delete</th>
+            </>
+          )}
         </tr>
       </thead>
       <tbody>
-        {data.map((row, rowIndex) => (
-          <tr key={rowIndex}>
+        {data.map((row) => (
+          <tr key={row._id}>
             {headers.map((header, colIndex) => (
-              <td key={colIndex}>{row[header]}</td>
+              <td key={colIndex}>
+                {row[headerToKeyMapping[header]]}
+              </td>
             ))}
+            {opt === 0 && (
+              <td>
+                <button onClick={() => onUpdate(row._id)}>Edit</button>
+              </td>
+            )}
+            {opt === 1 && (
+              <td>
+                <button onClick={() => onDelete(row._id)}>Delete</button>
+              </td>
+            )}
+            {opt === 2 && (
+              <>
+                <td>
+                  <button onClick={() => onUpdate(row._id)}>Edit</button>
+                </td>
+                <td>
+                  <button onClick={() => onDelete(row._id)}>Delete</button>
+                </td>
+              </>
+            )}
           </tr>
         ))}
       </tbody>
