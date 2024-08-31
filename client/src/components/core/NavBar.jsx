924b4179697a93c624b6778a0e012575c8778f29
raveesha gimhan <146311393+R3aveesha@users.noreply.github.com>
2024-08-31 19:41:59 +0530

    updated

--- client/src/components/core/NavBar.jsx
+++ client/src/components/core/NavBar.jsx
@@ -55,7 +55,7 @@ const NavBar = ({ name }) => {
           Foods
         </Link>
         <Link
-          to="/events"
+          to="/eventdashboard"
           className={styles.link}
           style={{
             backgroundColor: currentPage === "events" ? "white" : "transparent",
--- client/src/pages/eventManagement/EventDashboard.jsx
+++ client/src/pages/eventManagement/EventDashboard.jsx
@@ -92,7 +92,7 @@ const EventDashboard = () => {
           )}
         </div>
       </div>
-      <Footer />
+      
     </>
   );
 };
